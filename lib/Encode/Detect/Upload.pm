6d2a770aa9962f3a4e27a5596c31a321864e2a20
Lyle Hopkins <cosmicnet@cpan.org>
2013-11-07 15:05:42 +0000

    Import of COSMICNET/Encode-Detect-Upload-0.03 from CPAN.
    gitpan-cpan-distribution: Encode-Detect-Upload
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         COSMICNET/Encode-Detect-Upload-0.03.tar.gz
    gitpan-cpan-author:       COSMICNET
    gitpan-cpan-maturity:     released

--- lib/Encode/Detect/Upload.pm
+++ lib/Encode/Detect/Upload.pm
@@ -1,6 +1,6 @@
 package Encode::Detect::Upload;
 
-our $VERSION=0.02;
+our $VERSION=0.03;
 
 =encoding utf8
 
--- lib/Encode/Detect/Upload/Data.pm
+++ lib/Encode/Detect/Upload/Data.pm
@@ -1,6 +1,6 @@
 package Encode::Detect::Upload::Data;
 
-our $VERSION=0.02;
+our $VERSION=0.03;
 
 =head1 NAME
 
